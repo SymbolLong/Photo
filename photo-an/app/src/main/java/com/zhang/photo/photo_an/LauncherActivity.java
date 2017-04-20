c3e085a6271627c411a677dd2a753498e72b64f1
SymbolLong <877857078@qq.com>
2017-04-20 18:11:55 +0800

    闪屏代码
    添加闪屏代码，环境有问题，回家解决吧

---
+++ photo-an/app/src/main/java/com/zhang/photo/photo_an/LauncherActivity.java
@@ -0,0 +1,48 @@
+package com.zhang.photo.photo_an;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.graphics.PixelFormat;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+/**
+ * Created by zhangsl on 2017/4/20.
+ */
+public class LauncherActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        getWindow().setFormat(PixelFormat.RGBA_8888);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DITHER);
+
+        setContentView(R.layout.activity_launcher);
+
+        //Display the current version number
+        PackageManager pm = getPackageManager();
+        try {
+            PackageInfo pi = pm.getPackageInfo("org.wordpress.android", 0);
+            TextView versionNumber = (TextView) findViewById(R.id.versionNumber);
+            versionNumber.setText("Version " + pi.versionName);
+        } catch (NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                /* Create an Intent that will start the Main WordPress Activity. */
+                Intent mainIntent = new Intent(LauncherActivity.this, MainActivity.class);
+                LauncherActivity.this.startActivity(mainIntent);
+                LauncherActivity.this.finish();
+            }
+        }, 3000); //2900 for release
+
+    }
+
+}
