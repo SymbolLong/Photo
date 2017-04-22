12e9976e45fc049d58f8c95fab0664fe6d59da8d
SymbolLong <877857078@qq.com>
2017-04-22 10:59:51 +0800

    AN添加手势识别

--- photo-an/app/src/main/java/com/zhang/photo/photo_an/MainActivity.java
+++ photo-an/app/src/main/java/com/zhang/photo/photo_an/MainActivity.java
@@ -2,12 +2,48 @@ package com.zhang.photo.photo_an;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+    private ImageView imgView;
+    //手指按下的点为(x1, y1)手指离开屏幕的点为(x2, y2)
+    float x1 = 0;
+    float x2 = 0;
+    float y1 = 0;
+    float y2 = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imgView = (ImageView) findViewById(R.id.imgView);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        //继承了Activity的onTouchEvent方法，直接监听点击事件
+        if(event.getAction() == MotionEvent.ACTION_DOWN) {
+            //当手指按下的时候
+            x1 = event.getX();
+            y1 = event.getY();
+        }
+        if(event.getAction() == MotionEvent.ACTION_UP) {
+            //当手指离开的时候
+            x2 = event.getX();
+            y2 = event.getY();
+            if(y1 - y2 > 50) {
+                Toast.makeText(MainActivity.this, "向上滑", Toast.LENGTH_SHORT).show();
+            } else if(y2 - y1 > 50) {
+                Toast.makeText(MainActivity.this, "向下滑", Toast.LENGTH_SHORT).show();
+            } else if(x1 - x2 > 50) {
+                Toast.makeText(MainActivity.this, "向左滑", Toast.LENGTH_SHORT).show();
+            } else if(x2 - x1 > 50) {
+                Toast.makeText(MainActivity.this, "向右滑", Toast.LENGTH_SHORT).show();
+            }
+        }
+        return super.onTouchEvent(event);
     }
 }
